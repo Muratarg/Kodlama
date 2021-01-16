f6abc3714442d5b13104cf25a43010df6caac527
Murat Argun <murat.argun1903@hotmail.com>
2021-01-17 00:00:38 +0300

    Add project files.

---
+++ Kodlama/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Kodlama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
